df7c7d316e99fc13744d2d169dfdc5fa46a0673e
Eduardo Acosta <eacostaserretcarrill@deloitte.com>
2024-08-23 10:06:03 -0600

    Added findBy email and names, added specific endpoints

--- src/main/java/com/sebas/shoppingcart/controllers/UserController.java
+++ src/main/java/com/sebas/shoppingcart/controllers/UserController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sebas.shoppingcart.model.User;
@@ -34,18 +34,33 @@ public class UserController {
 		return repository.findAll();
 	}
 	
-	//NEEDS TO BE SENT ALL VALUES TO CREATE A USER (EMAIL,FIRSTNAME,LASTNAME,AREAOFINTERES)
-	@RequestMapping(value="/userBy",method=RequestMethod.GET)
-	public ResponseEntity<?> getUserByName(@RequestBody User user) {
-		List<User> allUsers = repository.findAll();
-		for(User u:allUsers) {
-			if((user.getFirstName().equals(u.getFirstName()) && user.getLastName().equals(u.getLastName()))
-					|| user.getEmail().equals(u.getEmail())) {
-				return ResponseEntity.status(HttpStatus.FOUND).body(u);
-			}
+	@RequestMapping(value="/useremail",method=RequestMethod.GET)
+	public ResponseEntity<?> getUserByName(@RequestParam(value="email") String email) {
+		Optional<User> userWithEmail = repository.findByEmail(email);
+		if(userWithEmail.isPresent()) {
+			return ResponseEntity.status(HttpStatus.FOUND).body(userWithEmail);
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No users found with that info");
 	}
+	
+	@RequestMapping(value="/username",method=RequestMethod.GET)
+	public ResponseEntity<?> getUserByInfo(@RequestParam(value="lastName",required=false) String lastName,
+											@RequestParam(value="firstName",required=false) String firstName) {
+		List<User> listFirst = repository.findByFirstName(firstName);
+		List<User> listLast = repository.findByLastName(lastName);
+		if(!listFirst.isEmpty() && !listLast.isEmpty()) {
+			for(User u:listLast) {
+				if(listFirst.contains(u)) {
+					return ResponseEntity.status(HttpStatus.FOUND).body(u);
+				}
+			}
+		}
+		else if((!listFirst.isEmpty() && listLast.isEmpty()) || (listFirst.isEmpty() && !listLast.isEmpty())) {
+			return listFirst.isEmpty() ? (ResponseEntity.status(HttpStatus.FOUND).body(listLast)) : ResponseEntity.status(HttpStatus.FOUND).body(listFirst);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No users found with that info");
+	}
+	
 	
 	@RequestMapping(value="/saveUser",method=RequestMethod.POST)
 	public ResponseEntity<?> saveUser(@RequestBody User user) {
--- src/main/java/com/sebas/shoppingcart/repos/UserRepository.java
+++ src/main/java/com/sebas/shoppingcart/repos/UserRepository.java
@@ -1,9 +1,14 @@
 package com.sebas.shoppingcart.repos;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sebas.shoppingcart.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	
+	Optional<User> findByEmail(String email);
+	List<User> findByFirstName(String firstName);
+	List<User> findByLastName(String lastName);
 }
